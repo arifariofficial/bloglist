cab2c2c06f91d01c26eb85b15f3bc71c97a7eb26
Ariful Islam <ariful6@yahoo.com>
2023-12-22 05:04:53 +0200

    awaitm mongoose connect

--- controllers/testingService.js
+++ controllers/testingService.js
@@ -1,10 +1,12 @@
 const testingService = require("express").Router();
+const { Mongoose } = require("mongoose");
+const { connect } = require("../app");
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
 testingService.post("/reset", async (request, response) => {
   try {
-    await connectDB();
+    await Mongoose.connect();
     await Blog.deleteMany({});
     await User.deleteMany({});
 
